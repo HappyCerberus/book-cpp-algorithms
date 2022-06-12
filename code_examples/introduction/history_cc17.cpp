b4d363e8b11b73c8667452e17881faafbae5cbb8
RNDr. Simon Toth <business@simontoth.eu>
2022-06-12 20:34:12 +0200

    Foreach example for parallel algorithms.

---
+++ code_examples/introduction/history_cc17.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <atomic>
+#include <execution>
+
+int main() {
+#include "history_cc17_code.h"
+assert(cnt == 9);
+assert(sum == 45);
+std::cout << ".";
+}
---
+++ code_examples/introduction/history_cc17_code.h
@@ -0,0 +1,9 @@
+std::atomic<int> cnt = 0, sum = 0;
+std::vector<int> data = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+std::for_each(std::execution::par_unseq,
+	data.begin(), data.end(), 
+	[&](int el) {
+		cnt++;
+		sum += el;
+	});
+// cnt == 9, sum == 45
