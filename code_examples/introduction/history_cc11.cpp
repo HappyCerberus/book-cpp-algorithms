614a3b069b3f813bf45773a4f8fcd8f71ab68eb6
RNDr. Simon Toth <business@simontoth.eu>
2022-06-12 16:07:00 +0200

    C++11 foreach example.

---
+++ code_examples/introduction/history_cc11.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+
+int main() {
+#include "history_cc11_code.h"
+
+assert(cnt == 9);
+assert(sum == 45);
+
+std::cout << ".";
+}
