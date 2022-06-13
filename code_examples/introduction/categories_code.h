c1be068d1a86f0720d90a9b0ad75a0406a6be6d3
RNDr. Simon Toth <business@simontoth.eu>
2022-06-13 13:40:30 +0200

    Changed iterator categories example.

--- code_examples/introduction/categories_code.h
+++ code_examples/introduction/categories_code.h
@@ -3,16 +3,15 @@ auto it1 = arr.begin();
 it1 += 5; // OK, std::vector provides random access iterator
 ++it1; // OK, all iterators provide advance operation
 
-ssize_t dst1 = it1 - arr.begin();
+ssize_t dst1 = it1 - arr.begin(); // OK, random access iterator
 // dst1 == 6
 
 std::list<int> lst = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 auto it2 = lst.begin();
 // it2 += 5; Would not compile.
-std::advance(it2, 5); // Linear advance by 5 steps
+std::advance(it2, 5); // OK, linear advance by 5 steps
 ++it2; // OK, all iterators provide advance operation
 
 // it2 - lst.begin(); Would not compile
-ssize_t dst2 = std::distance(lst.begin(), it2);
-// Linear calculation of distance
+ssize_t dst2 = std::distance(lst.begin(), it2); // OK, linear calculation
 // dst2 == 6
