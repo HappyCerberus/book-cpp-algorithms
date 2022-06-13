cfb2660bda73e253a99e5938b7f72897df71b9b6
RNDr. Simon Toth <business@simontoth.eu>
2022-06-13 13:37:49 +0200

    Example for iterator categories.

---
+++ code_examples/introduction/categories.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+
+int main() {
+#include "categories_code.h"
+assert(dst1 == 6);
+assert(dst2 == 6);
+std::cerr << ".";
+}
---
+++ code_examples/introduction/categories_code.h
@@ -0,0 +1,18 @@
+std::vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+auto it1 = arr.begin();
+it1 += 5; // OK, std::vector provides random access iterator
+++it1; // OK, all iterators provide advance operation
+
+ssize_t dst1 = it1 - arr.begin();
+// dst1 == 6
+
+std::list<int> lst = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+auto it2 = lst.begin();
+// it2 += 5; Would not compile.
+std::advance(it2, 5); // Linear advance by 5 steps
+++it2; // OK, all iterators provide advance operation
+
+// it2 - lst.begin(); Would not compile
+ssize_t dst2 = std::distance(lst.begin(), it2);
+// Linear calculation of distance
+// dst2 == 6
