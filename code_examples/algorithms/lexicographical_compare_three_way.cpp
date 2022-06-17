81dec73616e78cfbd0764b8a3035ec11888265a9
RNDr. Simon Toth <business@simontoth.eu>
2022-06-17 18:58:52 +0200

    Example for lexicographical_compare_three_way

---
+++ code_examples/algorithms/lexicographical_compare_three_way.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <cassert>
+#include <concepts>
+#include <vector>
+#include <iostream>
+
+int main() {
+#include "lexicographical_compare_three_way_code.h"
+assert(cmp == std::strong_ordering::less);
+
+std::cerr << ".";
+}
---
+++ code_examples/algorithms/lexicographical_compare_three_way_code.h
@@ -0,0 +1,7 @@
+std::vector<int> data1 = { 1, 1, 1};
+std::vector<int> data2 = { 1, 2, 3};
+
+auto cmp = std::lexicographical_compare_three_way(
+    data1.begin(), data1.end(), 
+    data2.begin(), data2.end());
+// cmp == std::strong_ordering::less
