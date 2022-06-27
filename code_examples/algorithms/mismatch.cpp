976047faea4a4e6db81bd743d669a21983391752
RNDr. Simon Toth <business@simontoth.eu>
2022-06-27 13:14:34 +0200

    Example for mismatch.

---
+++ code_examples/algorithms/mismatch.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+int main() {
+#include "mismatch_code.h"
+assert(it_pair.first == first.begin()+2);
+assert(it_pair.second == second.begin()+2);
+
+std::cerr << ".";
+}
