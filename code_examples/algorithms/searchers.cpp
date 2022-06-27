ecfecca22c9a5e5ff04600c8267f1cc897317449
RNDr. Simon Toth <business@simontoth.eu>
2022-06-27 11:56:38 +0200

    Example for custom searchers.

---
+++ code_examples/algorithms/searchers.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <list>
+#include <span>
+#include <functional>
+
+int main() {
+#include "searchers_code.h"
+assert(it1 == haystack.begin()+1);
+assert(it2 == haystack.begin()+1);
+assert(it3 == haystack.begin()+1);
+
+std::cerr << ".";
+}
---
+++ code_examples/algorithms/searchers_code.h
@@ -0,0 +1,14 @@
+std::string haystack = "abbabba";
+std::string needle = "bba";
+
+auto it1 = std::search(haystack.begin(), haystack.end(), 
+	std::default_searcher(needle.begin(), needle.end()));
+
+auto it2 = std::search(haystack.begin(), haystack.end(), 
+	std::boyer_moore_searcher(needle.begin(), needle.end()));
+
+auto it3 = std::search(haystack.begin(), haystack.end(), 
+	std::boyer_moore_horspool_searcher(needle.begin(), needle.end()));
+// it1 == it2 == it3
+
+
