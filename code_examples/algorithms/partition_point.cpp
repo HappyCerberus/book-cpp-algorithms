b95e742efae0a1c856ec3490b720a34f131cae78
RNDr. Simon Toth <business@simontoth.eu>
2022-06-29 16:15:32 +0200

    Example for partition_piont

---
+++ code_examples/algorithms/partition_point.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <set>
+
+int main() {
+#include "partition_point_code.h"
+assert(*pp == 5);
+
+std::cerr << ".";
+}
