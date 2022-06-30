07275b8a7407515340f172a6c58298232ff494e5
RNDr. Simon Toth <business@simontoth.eu>
2022-06-30 12:28:20 +0200

    Example for includes.

---
+++ code_examples/algorithms/includes.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+
+int main() {
+#include "includes_code.h"
+assert(test);
+
+std::cerr << ".";
+}
---
+++ code_examples/algorithms/includes_code.h
@@ -0,0 +1,8 @@
+std::vector<char> letters('z'-'a'+1,'\0');
+std::iota(letters.begin(), letters.end(), 'a');
+
+std::string input = "the quick brown fox jumps over the lazy dog";
+std::ranges::sort(input);
+
+bool test = std::ranges::includes(input, letters);
+// test == true
