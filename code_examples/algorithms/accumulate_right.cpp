e4dc241eaa14d421c189a4bc3b881cfdfa0ff553
RNDr. Simon Toth <business@simontoth.eu>
2022-07-01 21:12:38 +0200

    Examples for accumulate.

---
+++ code_examples/algorithms/accumulate_right.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+#include <ranges>
+#include <execution>
+
+int main() {
+#include "accumulate_right_code.h"
+assert(left_fold == 8);
+assert(right_fold == 3);
+
+std::cerr << ".";
+}
---
+++ code_examples/algorithms/accumulate_right_code.h
@@ -0,0 +1,14 @@
+std::vector<int> data{1, 2, 3, 4, 5};
+
+auto left_fold = std::accumulate(data.begin(), data.end(), 0,
+	[](int acc, int el) {
+		return acc / 2 + el;
+	});
+// left_fold == 8
+
+auto right_fold = std::accumulate(data.rbegin(), data.rend(), 0,
+	[](int acc, int el) {
+		return acc / 2 + el;
+	});
+// right_fold == 3
+
