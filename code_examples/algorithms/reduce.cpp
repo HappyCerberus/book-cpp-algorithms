14a90756ab169dc14b873c94b79734a00cc05695
RNDr. Simon Toth <business@simontoth.eu>
2022-07-03 09:45:57 +0200

    Example for reduce.

---
+++ code_examples/algorithms/reduce.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+#include <ranges>
+#include <execution>
+
+int main() {
+#include "reduce_code.h"
+assert(sum == 15);
+assert(product == 120);
+
+std::cerr << ".";
+}
---
+++ code_examples/algorithms/reduce_code.h
@@ -0,0 +1,15 @@
+std::vector<int> data{1, 2, 3, 4, 5};
+
+auto sum = std::reduce(data.begin(), data.end(), 0);
+// sum == 15
+
+sum = std::reduce(std::execution::par_unseq,
+	data.begin(), data.end(), 0);
+// sum == 15
+
+auto product = std::reduce(data.begin(), data.end(), 1, std::multiplies<>{});
+// product == 120
+
+product = std::reduce(std::execution::par_unseq,
+	data.begin(), data.end(), 1, std::multiplies<>{});
+// product == 120
