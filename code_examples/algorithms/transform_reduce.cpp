547c52dce6fbc34fdae37651ed73b653cd0ec5e5
RNDr. Simon Toth <business@simontoth.eu>
2022-07-03 10:36:39 +0200

    Example for transform_reduce.

---
+++ code_examples/algorithms/transform_reduce.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+#include <ranges>
+#include <execution>
+
+int main() {
+#include "transform_reduce_code.h"
+assert(sum_of_squares == 55);
+assert(result == 3);
+
+std::cerr << ".";
+}
---
+++ code_examples/algorithms/transform_reduce_code.h
@@ -0,0 +1,9 @@
+std::vector<int> data{1, 2, 3, 4, 5};
+auto sum_of_squares = std::transform_reduce(data.begin(), data.end(), 0, 
+	std::plus<>{}, [](int v) { return v*v; });
+// sum_of_squares == 55
+
+std::vector<int> coef{1, -1, 1, -1, 1};
+auto result = std::transform_reduce(data.begin(), data.end(), 
+	coef.begin(), 0);
+// result == 1*1 + 2*(-1) + 3*1 + 4*(-1) + 5*1 == 3
