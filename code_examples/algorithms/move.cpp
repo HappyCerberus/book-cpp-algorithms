e10fc09ec9158d5f2498e06bc61f072e908c5a38
RNDr. Simon Toth <business@simontoth.eu>
2022-07-04 09:54:37 +0200

    Example for std::move.

---
+++ code_examples/algorithms/move.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+#include <ranges>
+#include <execution>
+#include <cmath>
+
+int main() {
+#include "move_code.h"
+std::vector<std::string> cmp{ "", "", "", "a", "b", "c" };
+assert(std::ranges::equal(data, cmp));
+
+std::cerr << ".";
+}
