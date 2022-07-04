63b3f59fffd089eaa6b79b9c390f309f527020c8
RNDr. Simon Toth <business@simontoth.eu>
2022-07-04 11:06:13 +0200

    Example for replace_copy and replace_copy_if.

---
+++ code_examples/algorithms/replace_copy.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+#include <ranges>
+#include <execution>
+#include <cmath>
+#include <list>
+#include <random>
+
+int main() {
+#include "replace_copy_code.h"
+auto cmp1 = { 1, 2, 3, 4, 10, 6, 7, 8, 9 };
+auto cmp2 = { 1, -1, 3, -1, 5, -1, 7, -1, 9 };
+assert(std::ranges::equal(out, cmp1));
+assert(std::ranges::equal(odd, cmp2));
+
+
+std::cerr << ".";
+}
