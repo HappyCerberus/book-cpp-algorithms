36243a0b2d89abeb0a1a8c8fdb8e239825a618ec
RNDr. Simon Toth <business@simontoth.eu>
2022-07-04 12:23:00 +0200

    Example for rotate_copy.

---
+++ code_examples/algorithms/rotate_copy.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <ranges>
+#include <numeric>
+#include <ranges>
+#include <execution>
+#include <cmath>
+#include <list>
+#include <random>
+
+int main() {
+#include "rotate_copy_code.h"
+auto cmp = { 5, 6, 7, 8, 9, 1, 2, 3, 4 };
+assert(std::ranges::equal(out, cmp));
+
+std::cerr << ".";
+}
