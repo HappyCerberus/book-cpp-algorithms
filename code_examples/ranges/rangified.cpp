9fc9cb2d21f358e6b3f91477f990a86847337f3f
RNDr. Simon Toth <business@simontoth.eu>
2022-07-22 13:21:17 +0200

    Example of rangified sort.

---
+++ code_examples/ranges/rangified.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iterator>
+#include <concepts>
+#include <ranges>
+#include <list>
+#include <cassert>
+#include <algorithm>
+#include <random>
+
+int main() {
+#include "rangified_code.h"
+auto cmp = {1, 2, 3, 4};
+assert(std::ranges::equal(cmp, dt));
+}
---
+++ code_examples/ranges/rangified_code.h
@@ -0,0 +1,2 @@
+std::vector<int> dt = {1, 4, 2, 3};
+std::ranges::sort(dt);
