8f6ab5889a1d4277f0b047ac5b77d717e621feb9
RNDr. Simon Toth <business@simontoth.eu>
2022-07-29 10:54:48 +0200

    Example for views::reverse

---
+++ code_examples/views/reverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <ranges>
+#include <unordered_map>
+#include <vector>
+#include <cassert>
+
+int main() {
+#include "reverse_code.h"
+    auto cmp1 = {4, 3, 2, 1};
+    assert(std::ranges::equal(out, cmp1));
+}
