71d698a2d71a66480112d46eeac69d0dbdbc0486
RNDr. Simon Toth <business@simontoth.eu>
2022-07-30 13:17:47 +0200

    Example for istream view.

---
+++ code_examples/views/istream.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+#include <ranges>
+#include <vector>
+#include <cassert>
+
+int main() {
+#include "istream_code.h"
+}
---
+++ code_examples/views/istream_code.h
@@ -0,0 +1,3 @@
+std::ranges::for_each(std::views::istream<int>(std::cin), [](int v) {
+	// iterate over integers on standard input
+});   
