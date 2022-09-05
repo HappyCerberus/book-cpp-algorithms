b6ce9b2d6e0945cf963d0a6418564d16a26c10a7
RNDr. Simon Toth <business@simontoth.eu>
2022-09-05 20:12:49 +0200

    Example for input ranges.

---
+++ code_examples/introduction/mental_range.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+int main() {
+#include "mental_range_code.h"
+assert(std::ranges::equal(data, out));
+std::cerr << ".";
+}
---
+++ code_examples/introduction/mental_range_code.h
@@ -0,0 +1,7 @@
+std::vector<int> data{1, 2, 3, 4, 5, 6, 7};
+std::vector<int> out(7,0);
+
+std::copy(data.begin(), data.end(), // input range
+    out.begin() // output range, end iterator is implied:
+    // std::next(out.begin(), std::distance(data.begin(), data.end()));
+);
