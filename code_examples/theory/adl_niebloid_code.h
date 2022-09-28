cf6f83fb09d2fc32945e506f8ff402b7f41e623c
RNDr. Simon Toth <business@simontoth.eu>
2022-09-28 18:38:43 +0200

    Formatting fix.

--- code_examples/theory/adl_niebloid_code.h
+++ code_examples/theory/adl_niebloid_code.h
@@ -6,8 +6,8 @@ namespace impl {
     struct DoSomethingFn { 
         template <typename T> void operator()(T&& arg) const
         requires HasCustomImpl<T> {
-		do_something(std::forward<T>(arg));
-	}
+            do_something(std::forward<T>(arg));
+        }
 
         template <typename T> void operator()(T&&) const
         requires (!HasCustomImpl<T>) { /* default implementation */ }
